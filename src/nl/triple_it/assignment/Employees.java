d6f3622fc22683757465d014b241dc67660f103d
Lowerland <Lowerland@hotmail.com>
2014-07-15 19:10:43 +0200

    Formatted code

--- src/nl/triple_it/assignment/Employees.java
+++ src/nl/triple_it/assignment/Employees.java
@@ -1,19 +1,3 @@
-/*
- *  Copyright 2013-2014 Jeroen Gorter <Lowerland@hotmail.com>
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 package nl.triple_it.assignment;
 
 public class Employees {
@@ -22,20 +6,18 @@ public class Employees {
 	private String lastname;
 	private String emailaddress;
 	private String photourl;
-	
-	public Employees(){
-		
+
+	public Employees() {
 	}
 
-	public Employees(String firstname, String lastname, String emailaddress, String photourl){
+	public Employees(String firstname, String lastname, String emailaddress, String photourl) {
 		super();
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.emailaddress = emailaddress;
 		this.photourl = photourl;
-		
 	}
-	
+
 	public String getFirstname() {
 		return firstname;
 	}
@@ -67,6 +49,5 @@ public class Employees {
 	public void setPhotourl(String photourl) {
 		this.photourl = photourl;
 	}
-	
-	
+
 }
